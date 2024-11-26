52650adb88ce749877eafd760798f25b4f812b72
RusselxD <russelcabigquez8@gmail.com>
2024-11-27 03:29:53 +0800

    progress for reports control

--- StaffControl.xaml.cs
+++ StaffControl.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,8 +14,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Airport_Management_System
 {
@@ -24,11 +27,375 @@ namespace Airport_Management_System
     public partial class StaffControl : UserControl
     {
         private List<List<string>> staffs;
+        private List<List<string>> filteredStaffs;
+        private bool filterIsOn;
+
+        private bool roleFilterIsOn;
+        private bool shiftFilterIsOn;
+        private bool statusFilterIsOn;
+
+        // -------------------------------------- STAFF BORDER REUSABLE COMPONENTS -------------------------------------- //
+        private Thickness borderThickness = new Thickness(0, 0, 0, 1);
+        private Brush black = Brushes.Black;
+
+        private Thickness pad1 = new Thickness(20, 17, 0, 0);
+        private Thickness pad2 = new Thickness(0, 17, 0, 0);
+        private Thickness pad3 = new Thickness(10, 12, 0, 0);
+
+        private Thickness statusBorderMargin = new Thickness(0, 5, 5, 5);
+        private CornerRadius statusBorderRadius = new CornerRadius(5);
+
+        private SolidColorBrush green = new SolidColorBrush(Color.FromRgb(186, 255, 167));
+        private SolidColorBrush gray = new SolidColorBrush(Color.FromRgb(191, 191, 191));
+        private SolidColorBrush red = new SolidColorBrush(Color.FromRgb(255, 192, 192));
+        // --------------------------------------------------------------------------------------------------------------- //
 
         public StaffControl(SqlConnection sqlConnection)
         {
             staffs = new List<List<string>>();
+            filteredStaffs = new List<List<string>>();
+
+            roleFilterIsOn = false;
+            shiftFilterIsOn = false;
+            statusFilterIsOn = false;
+
+            filterIsOn = roleFilterIsOn || shiftFilterIsOn || statusFilterIsOn;
             InitializeComponent();
+
+            Task.Run(() => QueryStaffsTable(CancellationToken.None));
+
+            InitializeAnimations();
+        }
+
+        private void InitializeAnimations()
+        {
+            showRoles = new DoubleAnimation
+            {
+                Duration = new Duration(TimeSpan.FromMilliseconds(300))
+            };
+            showRoles.Completed += (s, args) =>
+            {
+                if (!rolesIsOpen)
+                {
+                    RoleDropDown.CornerRadius = new CornerRadius(8);
+                }
+            };
+
+            showShifts = new DoubleAnimation
+            {
+                Duration = new Duration(TimeSpan.FromMilliseconds(300))
+            };
+            showShifts.Completed += (s, args) =>
+            {
+                if (!shiftsIsOpen)
+                {
+                    ShiftsDropDown.CornerRadius = new CornerRadius(8);
+                }
+            };
+
+            showStatus = new DoubleAnimation
+            {
+                Duration = new Duration(TimeSpan.FromMilliseconds(300))
+            };
+            showStatus.Completed += (s, args) =>
+            {
+                if (!statusIsOpen)
+                {
+                    StatusDropDown.CornerRadius = new CornerRadius(8);
+                }
+            };
+        }
+
+        DoubleAnimation showRoles;
+        DoubleAnimation showShifts;
+        DoubleAnimation showStatus;
+
+        private void Get_Column_Definition(Grid g)
+        {
+            g.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1.15, GridUnitType.Star) });
+            g.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1.2, GridUnitType.Star) });
+            g.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            g.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(0.9, GridUnitType.Star) });
+        }
+
+        FontFamily ubuntu = new FontFamily("Ubuntu");
+
+        private TextBlock Make_TextBlock(string text, Thickness pad, int col)
+        {
+            TextBlock tb = new TextBlock()
+            {
+                Text = text,
+                FontSize = 19,
+                FontFamily = ubuntu,
+                Padding = pad
+            };
+            Grid.SetColumn(tb, col);
+
+            return tb;
+        }
+
+        private void Display_All_Staffs()
+        {
+            staffsPanel.Children.Clear();
+            int staffsDisplayed = 0;
+
+            foreach (List<string> staff in staffs)
+            {
+                if (staffsDisplayed >= 9)
+                {
+                    staffsBorder.Height += 55;
+                    outerMostGrid.Height += 55;
+                }
+
+                Border b = new Border()
+                {
+                    Height = 55,
+                    BorderThickness = borderThickness,
+                    BorderBrush = black
+                };
+
+                Grid g = new Grid();
+                Get_Column_Definition(g);
+
+                TextBlock name = Make_TextBlock(staff[0], pad1, 0);
+                TextBlock role = Make_TextBlock(staff[1], pad2, 1);
+                TextBlock shift = Make_TextBlock(staff[2], pad2, 2);
+
+                Border status = new Border()
+                {
+                    Margin = statusBorderMargin,
+                    CornerRadius = statusBorderRadius
+                };
+                Grid.SetColumn(status, 3);
+
+                switch (staff[3])
+                {
+                    case "On Duty":
+                        status.Background = green;
+                        break;
+                    case "Off Duty":
+                        status.Background = gray;
+                        break;
+                    default:
+                        status.Background = red;
+                        break;
+                }
+
+                TextBlock statusText = new TextBlock()
+                {
+                    Text = staff[3],
+                    FontSize = 19,
+                    FontFamily = ubuntu,
+                    Padding = pad3
+                };
+
+                status.Child = statusText;
+
+                g.Children.Add(name);
+                g.Children.Add(role);
+                g.Children.Add(shift);
+                g.Children.Add(status);
+
+                b.Child = g;
+
+                staffsPanel.Children.Add(b);
+                staffsDisplayed++;
+            }
+        }
+
+        private void Display_Filtered_Staffs()
+        {
+            staffsPanel.Children.Clear();
+            int staffsDisplayed = 0;
+         
+            foreach (List<string> staff in filteredStaffs)
+            {
+                if (staffsDisplayed >= 9)
+                {
+                    staffsBorder.Height += 55;
+                    outerMostGrid.Height += 55;
+                }
+
+                Border b = new Border()
+                {
+                    Height = 55,
+                    BorderThickness = borderThickness,
+                    BorderBrush = black
+                };
+
+                Grid g = new Grid();
+                Get_Column_Definition(g);
+
+                TextBlock name = Make_TextBlock(staff[0], pad1, 0);
+                TextBlock role = Make_TextBlock(staff[1], pad2, 1);
+                TextBlock shift = Make_TextBlock(staff[2], pad2, 2);
+
+                Border status = new Border()
+                {
+                    Margin = statusBorderMargin,
+                    CornerRadius = statusBorderRadius
+                };
+                Grid.SetColumn(status, 3);
+
+                switch (staff[3])
+                {
+                    case "On Duty":
+                        status.Background = green;
+                        break;
+                    case "Off Duty":
+                        status.Background = gray;
+                        break;
+                    default:
+                        status.Background = red;
+                        break;
+                }
+
+                TextBlock statusText = new TextBlock()
+                {
+                    Text = staff[3],
+                    FontSize = 19,
+                    FontFamily = ubuntu,
+                    Padding = pad3
+                };
+
+                status.Child = statusText;
+
+                g.Children.Add(name);
+                g.Children.Add(role);
+                g.Children.Add(shift);
+                g.Children.Add(status);
+
+                b.Child = g;
+
+                staffsPanel.Children.Add(b);
+                staffsDisplayed++;
+            }
+        }
+
+        private void Filter_Staffs()
+        {
+            filteredStaffs.Clear();
+
+            if (roleFilterIsOn)
+            {
+                string role = roleFilter.Text;
+                foreach (List<string> staff in staffs)
+                {
+                    if (staff[1] == role)
+                    {
+                        filteredStaffs.Add(staff);
+                    }
+                }
+
+                List<List<string>> temp = new List<List<string>>(filteredStaffs);
+
+                if (shiftFilterIsOn)
+                {
+                    string shift = shiftFilter.Text;
+                    foreach (List<string> staff in filteredStaffs)
+                    {
+                        if (staff[2] != shift)
+                        {
+                            temp.Remove(staff);
+                        }
+                    }
+                }
+
+                if (statusFilterIsOn)
+                {
+                    string status = statusFilter.Text;
+                    foreach (List<string> staff in filteredStaffs)
+                    {
+                        if (staff[3] != status)
+                        {
+                            temp.Remove(staff);
+                        }
+                    }
+                }
+
+                filteredStaffs = temp;
+            }
+            else if (shiftFilterIsOn)
+            {
+                string shift = shiftFilter.Text;
+                foreach (List<string> staff in staffs)
+                {
+                    if (staff[2] == shift)
+                    {
+                        filteredStaffs.Add(staff);
+                    }
+                }
+
+                List<List<string>> temp = new List<List<string>>(filteredStaffs);
+                if (statusFilterIsOn)
+                {
+                    string status = statusFilter.Text;
+                    foreach (List<string> staff in filteredStaffs)
+                    {
+                        if (staff[3] != status)
+                        {
+                            temp.Remove(staff);
+                        }
+                    }
+                }
+                filteredStaffs = temp;
+            }
+            else
+            {
+                string status = statusFilter.Text;
+                foreach (List<string> staff in staffs)
+                {
+                    if (staff[3] == status)
+                    {
+                        filteredStaffs.Add(staff);
+                    }
+                }
+            }
+        }
+
+        private void Display_Staff()
+        {
+            filterIsOn = roleFilterIsOn || shiftFilterIsOn || statusFilterIsOn;
+
+            staffsBorder.Height = 550;
+            outerMostGrid.Height = 741;
+            if (filterIsOn)
+            {
+                Filter_Staffs();
+                Display_Filtered_Staffs();
+            }
+            else
+            {
+                Display_All_Staffs();
+            }
+        }
+
+        private async Task QueryStaffsTable(CancellationToken token)
+        {
+            SqlDataReader staffsReader = null;
+            SqlCommand staffsQuery = new SqlCommand("SELECT * FROM staffs_table", MainWindow.sqlConnection);
+
+            using (staffsReader = await staffsQuery.ExecuteReaderAsync(token))
+            {
+                while (staffsReader.Read())
+                {
+                    List<string> staff = new List<string>();
+                    staff.Add(staffsReader[1].ToString()); // name
+                    staff.Add(staffsReader[2].ToString()); // role
+                    staff.Add(staffsReader[3].ToString()); // shift
+                    staff.Add(staffsReader[4].ToString()); // status
+
+                    staffs.Add(staff);
+                }
+            }
+
+            await Dispatcher.InvokeAsync(() =>
+            {
+                Display_Staff();
+            });
+
+            staffsReader?.Dispose();
+            staffsReader?.Close();
         }
 
         private void searchBox_GotFocus(object sender, RoutedEventArgs e)
@@ -48,23 +415,23 @@ namespace Airport_Management_System
 
         private void Role_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if(sender is TextBlock tb)
-            {
-                string role = tb.Text;
-                header.Text = role;
-            }
+            string role = (sender as TextBlock).Text;
+            roleFilter.Text = role;
+            roleFilterIsOn = role == "All" ? false : true;
+
+            showRoles.From = 138;
+            showRoles.To = 0;
+            rolesIsOpen = false;
+            Roles.BeginAnimation(HeightProperty, showRoles);
+
+            Display_Staff();
         }
 
         private bool rolesIsOpen = false;
 
         private void Role_DropDown_Click(object sender, MouseButtonEventArgs e)
         {
-            DoubleAnimation showRoles = new DoubleAnimation
-            {
-                Duration = new Duration(TimeSpan.FromMilliseconds(300)) 
-            };
-
-            if(rolesIsOpen)
+            if (rolesIsOpen)
             {
                 showRoles.From = 138;
                 showRoles.To = 0;
@@ -78,32 +445,27 @@ namespace Airport_Management_System
                 rolesIsOpen = true;
             }
 
-            showRoles.Completed += (s, args) =>
-            {
-                if (!rolesIsOpen)
-                {
-                    RoleDropDown.CornerRadius = new CornerRadius(8);
-                }
-            };
-
             Roles.BeginAnimation(HeightProperty, showRoles);
-
         }
 
         private void Shift_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            string shift = (sender as TextBlock).Text;
+            shiftFilter.Text = shift;
+            shiftFilterIsOn = shift == "All" ? false : true;
 
+            showShifts.From = 138;
+            showShifts.To = 0;
+            shiftsIsOpen = false;
+            Shifts.BeginAnimation(HeightProperty, showShifts);
+
+            Display_Staff();
         }
 
         private bool shiftsIsOpen = false;
 
         private void Shift_DropDown_Click(object sender, MouseButtonEventArgs e)
         {
-            DoubleAnimation showShifts = new DoubleAnimation
-            {
-                Duration = new Duration(TimeSpan.FromMilliseconds(300)) 
-            };
-
             if (shiftsIsOpen)
             {
                 showShifts.From = 138;
@@ -118,31 +480,27 @@ namespace Airport_Management_System
                 shiftsIsOpen = true;
             }
 
-            showShifts.Completed += (s, args) =>
-            {
-                if (!shiftsIsOpen)
-                {
-                    ShiftsDropDown.CornerRadius = new CornerRadius(8);
-                }
-            };
-
-            Shifts.BeginAnimation(HeightProperty, showShifts, HandoffBehavior.SnapshotAndReplace);            
+            Shifts.BeginAnimation(HeightProperty, showShifts, HandoffBehavior.SnapshotAndReplace);
         }
 
         private void Status_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            string status = (sender as TextBlock).Text;
+            statusFilter.Text = status;
+            statusFilterIsOn = status == "All" ? false : true;
 
+            showStatus.From = 138;
+            showStatus.To = 0;
+            statusIsOpen = false;
+            Status.BeginAnimation(HeightProperty, showStatus);
+
+            Display_Staff();
         }
 
         private bool statusIsOpen = false;
 
         private void Status_DropDown_Click(object sender, MouseButtonEventArgs e)
         {
-            DoubleAnimation showStatus = new DoubleAnimation
-            {
-                Duration = new Duration(TimeSpan.FromMilliseconds(300)) 
-            };
-
             if (statusIsOpen)
             {
                 showStatus.From = 138;
@@ -156,14 +514,6 @@ namespace Airport_Management_System
                 showStatus.To = 138;
                 statusIsOpen = true;
             }
-
-            showStatus.Completed += (s, args) =>
-            {
-                if (!statusIsOpen)
-                {
-                    StatusDropDown.CornerRadius = new CornerRadius(8);
-                }
-            };
 
             Status.BeginAnimation(HeightProperty, showStatus);
         }
